5087afd49fd4078426967fe0eddae8798fb02a06
mita <iaoiui0727@gmail.com>
2019-01-13 16:02:44 +0900

    Change Lambda Endpoint

--- deploy.sh
+++ deploy.sh
@@ -22,5 +22,5 @@ if [ `echo $?` == 0 ]; then
 REST_API_ID=`aws apigateway get-rest-apis | jq .items[0].id -r`
 REGION=`cat package.json | jq .config.region -r`
 echo  '\033[1;34mTry following command to confirm lambda fuction working correctly\033[0;39m'
-echo  '\033[1;34mcurl https://${REST_API_ID}.execute-api.${REGION}.amazonaws.com/prod/users -H 'accept: application/json' -s | jq .\033[0;39m'
+echo  '\033[1;34mcurl https://${REST_API_ID}.execute-api.${REGION}.amazonaws.com/prod/${endpoint} -H 'accept: application/json' -s | jq .\033[0;39m'
 fi
